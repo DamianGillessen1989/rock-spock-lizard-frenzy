1db6a94a4855fa0d460f3c65657a078c869d113c
Damian Gillessen <damian.gillessen.2009@alumni.nuim.ie>
2024-06-17 19:30:25 +0200

    Add current & final score variable & print results

--- run.py
+++ run.py
@@ -22,3 +22,18 @@ def main():
         else:
             print("Invalid input. Please enter a valid choice like 'Spock'.")
             print("And check your spelling!")
+
+        # Print current scores
+        print("\nCurrent Scores - Player: {}, Computer: {}".format(player_score, computer_score))
+
+    # Final scores and result
+    print("Final Scores - Player: {}, Computer: {}".format(player_score, computer_score))
+    if player_score > computer_score:
+        print("Congratulations! You won the game!")
+    elif player_score < computer_score:
+        print("Computer wins the game! Better luck next time!")
+    else:
+        print("It's a tie game! Well played!")
+
+if __name__ == "__main__":
+    main()
